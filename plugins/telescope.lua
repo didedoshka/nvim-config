00c525c05f482b2aa3c94b2001cbf55acb57b35b
didedoshka <didedoshka@gmail.com>
2023-05-27 12:43:04 +0300

    rewrote comments in telescope setup

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -13,13 +13,13 @@ return
   config = function()
     pcall(require('telescope').load_extension, 'fzf')
 
-    -- telescope recent
+    -- search recent
     vim.keymap.set('n', '<leader>sr', require('telescope.builtin').oldfiles)
-    -- telescope buffers
+    -- search buffers
     vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers)
-    -- telescope project files
+    -- search project files
     vim.keymap.set('n', '<leader>sp', require('telescope.builtin').find_files)
-    -- telescope by grep
+    -- search by grep
     vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep)
   end
 }
