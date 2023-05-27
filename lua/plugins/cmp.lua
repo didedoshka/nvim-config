ee8dbb6d1098034cddc100c6f6b1bbdbdc173e4e
didedoshka <didedoshka@gmail.com>
2023-05-28 01:18:58 +0300

    make cmp.lua look better
    change ' to ", remove unneccesary functions calls

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,44 +1,43 @@
 return {
-  'hrsh7th/nvim-cmp',
+  "hrsh7th/nvim-cmp",
   dependencies = {
-    'hrsh7th/cmp-nvim-lsp',
-    { 'hrsh7th/cmp-path', commit = "d83839a" },
-    'L3MON4D3/LuaSnip',
-    'saadparwaiz1/cmp_luasnip',
-    'rafamadriz/friendly-snippets'
+    "hrsh7th/cmp-nvim-lsp",
+    { "hrsh7th/cmp-path", commit = "d83839a" },
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets"
   },
 
 
   config = function()
-    capabilities = require('cmp_nvim_lsp').default_capabilities()
-
+    capabilities = require("cmp_nvim_lsp").default_capabilities() -- yes, it is global variable used in lua/lspconfig.lua
 
     require("luasnip.loaders.from_vscode").lazy_load()
-    --    require("luasnip").config.setup {}
 
-    require('cmp_path')
-
-    local cmp = require('cmp')
+    local cmp = require("cmp")
     cmp.setup({
       snippet = {
         expand = function(args)
-          require('luasnip').lsp_expand(args.body)
+          require("luasnip").lsp_expand(args.body)
         end,
       },
+
       window = {
         -- completion = cmp.config.window.bordered(),
         -- documentation = cmp.config.window.bordered(),
       },
+
       mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
       }),
+
       sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'path' },
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
       }
     })
   end
