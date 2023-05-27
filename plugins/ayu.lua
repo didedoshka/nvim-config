ac26bab6dca290519c88bafdeff4f093e475f73a
didedoshka <gorokhov.080805@gmail.com>
2023-05-27 12:32:23 +0300

    changed tabs to spaces

--- plugins/ayu.lua
+++ plugins/ayu.lua
@@ -1,14 +1,14 @@
 return
 {
-	"Shatur/neovim-ayu",
-	config = function()
-		local colors = require("ayu.colors")
-		colors.generate()
-		require("ayu").setup {
-			overrides = function()
-				return { LineNr = {fg = colors.comment} }
-			end
-		}
-		require("ayu").colorscheme()
-	end,
+  "Shatur/neovim-ayu",
+  config = function()
+    local colors = require("ayu.colors")
+    colors.generate()
+    require("ayu").setup {
+      overrides = function()
+        return { LineNr = { fg = colors.comment } }
+      end
+    }
+    require("ayu").colorscheme()
+  end,
 }
--- plugins/nerdtree.lua
+++ plugins/nerdtree.lua
@@ -1,21 +1,21 @@
 return
 {
-	"preservim/nerdtree",
-	config = function()
-		-- ".*/"
-		vim.keymap.set("n", "<leader>te", 
-		function() 
-			local path_with_file_name = vim.api.nvim_buf_get_name(0)
-			local path = path_with_file_name:match(".*/")
-			vim.cmd("edit " .. path)
-			vim.cmd("5")
-		end
-		) 
-    vim.keymap.set("n", "<leader>tp", 
-    function() 
-      vim.cmd("edit .")
-      vim.cmd("5")
-    end
-    ) 
-  end	
+  "preservim/nerdtree",
+  config = function()
+    -- ".*/"
+    vim.keymap.set("n", "<leader>te",
+      function()
+        local path_with_file_name = vim.api.nvim_buf_get_name(0)
+        local path = path_with_file_name:match(".*/")
+        vim.cmd("edit " .. path)
+        vim.cmd("5")
+      end
+    )
+    vim.keymap.set("n", "<leader>tp",
+      function()
+        vim.cmd("edit .")
+        vim.cmd("5")
+      end
+    )
+  end
 }
