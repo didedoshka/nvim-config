61ef5f76b081e640c568ae57a872bf6d0aa43437
didedoshka <didedoshka@gmail.com>
2023-09-30 23:14:17 +0300

    change settings

--- init.lua
+++ init.lua
@@ -1,65 +1,83 @@
 -- Created by didedoshka on May 24
 
--- setting leader
+-- set leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- setting indentation
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+-- set indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
--- setting number
+-- set number
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- command line only visible in command mode
--- vim.opt.cmdheight = 0
+-- sync clipboard between vim and os
+vim.opt.clipboard = "unnamedplus"
+
+-- set wrap and max text width
+vim.opt.wrap = false
+vim.opt.colorcolumn = "120"
+
+-- set extensions
+vim.filetype.add({ extension = { ["keymap"] = "cpp" } })
 
 -- installing lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- setting plugins
 require("lazy").setup(
 -- table of plugins
-  {
-    -- colorscheme
-    require("plugins.ayu"),
+    {
+        -- colorscheme
+        require("plugins.ayu"),
 
-    -- flit
-    require("plugins.flit"),
+        -- flit
+        require("plugins.flit"),
 
-    -- telescope
-    require("plugins.telescope"),
+        -- telescope
+        require("plugins.telescope"),
 
-    -- filetree
-    require("plugins.nerdtree"),
+        -- filetree
+        require("plugins.nerdtree"),
 
-    -- cmp
-    require("plugins.cmp"),
+        -- cmp
+        require("plugins.cmp"),
 
-    -- treesitter
-    require("plugins.treesitter"),
+        -- treesitter
+        require("plugins.treesitter"),
 
-    -- lspconfig
-    require("plugins.lspconfig"),
+        -- lspconfig
+        require("plugins.lspconfig"),
 
-    -- comment
-    require("plugins.comment"),
+        {
+            "jose-elias-alvarez/null-ls.nvim",
+            config = function()
+                require("null-ls").setup({
+                    sources = require("null-ls").builtins.formatting.autopep8.with({
+                        extra_args = { "--max-line-length", "119" }
+                    })
+                })
+            end,
+            dependencies = { "nvim-lua/plenary.nvim" }
+        },
+        -- comment
+        require("plugins.comment"),
 
-    -- lazygit
-    require("plugins.lazygit")
-  }
+        -- lazygit
+        require("plugins.lazygit")
+    }
 
 )
