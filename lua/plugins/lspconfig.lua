7a0ba97a50d8dc16380794ab439c44ce80131d96
didedoshka <didedoshka@gmail.com>
2024-03-30 02:17:43 +0300

    add missing lspconfig keymaps

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -74,6 +74,10 @@ return
 
                 -- basic keymaps
                 local opts = { buffer = args.buf }
+                vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+                vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+                vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+                vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
                 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
                 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
                 vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
