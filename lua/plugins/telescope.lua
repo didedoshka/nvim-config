0aad55a9795a039e499f3045d1f5b240a36e4b57
didedoshka <didedoshka@gmail.com>
2024-10-16 03:01:21 +0300

    codestyle

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,10 +19,13 @@ return
                 borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' }
             },
         }
-        vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, {desc = "look at open (b)uffers"})
-        vim.keymap.set('n', '<leader>o', require('telescope.builtin').find_files, {desc = "(o)pen file"})
-        vim.keymap.set('n', '<leader>h', require('telescope.builtin').live_grep, {desc = "grep in files. (h) is near f-find and g-grep"})
-        vim.keymap.set('n', '<leader>g', require('telescope.builtin').current_buffer_fuzzy_find, {desc = "(g)rep current buffer"})
-        vim.keymap.set('n', '<leader>s', require('telescope.builtin').lsp_document_symbols, {desc = "lsp document (s)ymbols"})
+        vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = "look at open (b)uffers" })
+        vim.keymap.set('n', '<leader>o', require('telescope.builtin').find_files, { desc = "(o)pen file" })
+        vim.keymap.set('n', '<leader>h', require('telescope.builtin').live_grep,
+            { desc = "grep in files. (h) is near f-find and g-grep" })
+        vim.keymap.set('n', '<leader>g', require('telescope.builtin').current_buffer_fuzzy_find,
+            { desc = "(g)rep current buffer" })
+        vim.keymap.set('n', '<leader>s', require('telescope.builtin').lsp_document_symbols,
+            { desc = "lsp document (s)ymbols" })
     end
 }
