0f9f9639f5d6db61edf4cfa52aac21c8ed6972c3
didedoshka <didedoshka@gmail.com>
2025-04-13 01:12:22 +0300

    disable treesitter textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,44 +8,11 @@ return
     config = function()
         require("nvim-treesitter").setup()
 
-        local interesting_objects = {
-            "@class.outer",
-            "@conditional.outer",
-            "@function.outer",
-            "@loop.outer",
-            "@return.outer",
-        }
-
         require('nvim-treesitter.configs').setup({
             auto_install = true,
             highlight = {
                 enable = true,
                 additional_vim_regex_highlighting = false,
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<leader>m",
-                    node_incremental = "<leader>m",
-                    scope_incremental = false,
-                    node_decremental = "<bs>",
-                },
-            },
-            textobjects = {
-                move = {
-                    enable = true,
-                    set_jumps = true, -- whether to set jumps in the jumplist
-                    goto_next_start = {
-                        ["<cr>"] = {
-                            query = interesting_objects
-                        },
-                    },
-                    goto_previous_start = {
-                        ["<bs>"] = {
-                            query = interesting_objects
-                        }
-                    },
-                },
             },
         })
     end
