cfb4e4e50925c448525b8e44b40562be7fdb1056
didedoshka <didedoshka@gmail.com>
2026-04-12 07:44:42 +0300

    treesitter: move to main

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,13 @@
 return
 {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter").setup()
-
-        require('nvim-treesitter.configs').setup({
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function(args)
+                pcall(vim.treesitter.start)
+                -- vim.bo[args.buf].indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
+            end,
         })
     end
 }
