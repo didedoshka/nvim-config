8bff434119638713c629a18e703d5bc5bb25a646
didedoshka <didedoshka@gmail.com>
2026-04-12 07:45:41 +0300

    snippets: add cpp vi and vvi

--- snippets/cpp.lua
+++ snippets/cpp.lua
@@ -1,5 +1,15 @@
 return {
     {
+        prefix = 'vi',
+        body = "vector<int>"
+    },
+
+    {
+        prefix = 'vvi',
+        body = "vector<vector<int>>"
+    },
+
+    {
         prefix = 'ft',
         body = "for ($1 $2 = $3; $2 < $4; ++$2) {\n\t$0\n}"
     },
@@ -42,6 +52,8 @@ return {
 #include <stack>
 #include <vector>
 
+#define int long long
+
 #ifdef DDD
 #define start              \
     cout.setf(ios::fixed); \
@@ -61,12 +73,6 @@ return {
 #endif
 
 using namespace std;
-
-using i32 = int32_t;
-using i64 = int64_t;
-using u32 = uint32_t;
-using u64 = uint64_t;
-using d80 = long double;
 
 // vector input
 template <typename T>
@@ -90,10 +96,86 @@ void solve() {
     $0
 }
 
-i32 main() {
+int32_t main() {
     start;
 
     solve();
+
+    finish;
+}
+]]
+    },
+
+    {
+        prefix = 'ttt',
+        body = [[
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <map>
+#include <numbers>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <vector>
+
+#define int long long
+
+#ifdef DDD
+#define start              \
+    cout.setf(ios::fixed); \
+    cout.precision(10);    \
+    int START = clock()
+#define finish                                                                  \
+    cout << "\ntime: " << (clock() - START) / (double)(CLOCKS_PER_SEC) << '\n'; \
+    return 0
+#else
+#define start                         \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);                   \
+    ios_base::sync_with_stdio(false); \
+    cout.setf(ios::fixed);            \
+    cout.precision(10)
+#define finish return 0
+#endif
+
+using namespace std;
+
+// vector input
+template <typename T>
+istream &operator>>(istream &is, vector<T> &vec) {
+    for (auto &i : vec) {
+        is >> i;
+    }
+    return is;
+}
+
+// vector output
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &vec) {
+    for (T i : vec) {
+        os << i << ' ';
+    }
+    return os;
+}
+
+void solve() {
+    $0
+}
+
+int32_t main() {
+    start;
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
 
     finish;
 }
