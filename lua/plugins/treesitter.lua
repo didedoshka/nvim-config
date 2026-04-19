a200c68675205abc8140a8434a24506605759937
didedoshka <didedoshka@gmail.com>
2026-04-20 00:21:10 +0300

    treesitter: auto-install parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,8 +5,13 @@ return
     config = function()
         vim.api.nvim_create_autocmd('FileType', {
             callback = function(args)
-                pcall(vim.treesitter.start)
-                -- vim.bo[args.buf].indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
+                local lang = vim.treesitter.language.get_lang(args.match)
+                local available_langs = require('nvim-treesitter').get_available()
+                local is_available = vim.tbl_contains(available_langs, lang)
+                if is_available then
+                    require('nvim-treesitter').install(lang):wait()
+                    vim.treesitter.start()
+                end
             end,
         })
     end
