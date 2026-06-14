436cfe629e38827bfa99946d58d76dd698b96970
didedoshka <didedoshka@gmail.com>
2026-06-14 22:18:34 +0300

    treesitter: add ya.make

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,22 @@
 return
 {
     "nvim-treesitter/nvim-treesitter",
+
+    -- dependencies = {
+    --     {
+    --         dir = yamake_treesitter_dir,
+    --         -- build = ":TSInstall yamake",
+    --         enabled = function()
+    --             local res = vim.uv.fs_stat(yamake_treesitter_dir)
+    --             vim.print(res)
+    --
+    --             return res ~= nil
+    --         end,
+    --         -- lazy = false,
+    --         config = function() end,
+    --     }
+    -- },
+
     build = ":TSUpdate",
     config = function()
         vim.api.nvim_create_autocmd('FileType', {
@@ -13,6 +29,29 @@ return
                     vim.treesitter.start()
                 end
             end,
+        })
+
+        vim.filetype.add({
+            filename = {
+                ["ya.make"] = "yamake",
+            },
+        })
+
+        local dir = vim.fn.expand("~/arc/devtools/ide/tree-sitter-yamake")
+        vim.api.nvim_create_autocmd('User', {
+            pattern = 'TSUpdate',
+            callback = function()
+                require('nvim-treesitter.parsers').yamake = {
+                    install_info = {
+                        path = dir,
+                        -- url = 'https://github.com/zimbulang/tree-sitter-zimbu',
+                        -- location = 'parser', -- only needed if the parser is in subdirectory of a "monorepo"
+                        -- generate = true, -- only needed if repo does not contain pre-generated `src/parser.c`
+                        -- generate_from_json = false, -- only needed if repo does not contain `src/grammar.json` either
+                        queries = 'queries/yamake', -- also install queries from given directory
+                    },
+                }
+            end
         })
     end
 }
