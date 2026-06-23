8aa3de2ede3f8a91eac3bdffdb6bbce0d5ba492c
didedoshka <didedoshka@gmail.com>
2026-06-24 00:41:35 +0300

    spider: delete

--- init.lua
+++ init.lua
@@ -314,16 +314,6 @@ require("lazy").setup({
     },
 
     {
-        "chrisgrieser/nvim-spider",
-        config = function()
-            vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>")
-            vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>")
-            vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>")
-            vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>")
-        end
-    },
-
-    {
         "ThePrimeagen/refactoring.nvim",
         dependencies = {
             "lewis6991/async.nvim",
